0ec6509b95c01994a14246f988baf0812daaef29
hyophyop <hyophyop@users.noreply.github.com>
2025-08-05 11:19:46 +0900

    Add example strategy template

---
+++ strategies/example_strategy/__init__.py
@@ -0,0 +1,9 @@
+class ExampleStrategy:
+    """Simple example strategy template."""
+
+    def __init__(self, config=None):
+        self.config = config or {}
+
+    def run(self):
+        """Execute the strategy logic."""
+        pass
