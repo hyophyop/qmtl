f8f9b8cf7beb5378cb52500a210b45fbb2c2bd04
hyophyop <hyophyop@users.noreply.github.com>
2025-08-17 19:59:36 +0900

    feat: add volume moving average and std transform

--- qmtl/qmtl/transforms/__init__.py
+++ qmtl/qmtl/transforms/__init__.py
@@ -3,5 +3,6 @@
 from .rate_of_change import rate_of_change
 from .stochastic import stochastic
 from .angle import angle
+from .volume_features import volume_features
 
-__all__ = ["rate_of_change", "stochastic", "angle"]
+__all__ = ["rate_of_change", "stochastic", "angle", "volume_features"]
